284b5812e73aa14c77fd9590ec6c1051c5648b55
Andrew Magid <magid.andrew@gmail.com>
2021-07-16 16:33:01 -0400

    more bug fixes + formatting

--- Structures.py
+++ Structures.py
@@ -7,13 +7,14 @@ class Block:
     def __str__(self):
         return "\n".join(["mining time: {}".format(self.mining_time), "timestamp: {}".format(self.timestamp)])
 
+
 class Blockchain:
     def __init__(self):
         self.chain: list[Block] = []
 
-    def addBlock(self, block: Block):
+    def add_block(self, block: Block):
         if len(self.chain) != 0:
-            block.timestamp = self.getGlobalTimeOfChain() + block.mining_time
+            block.timestamp = self.get_global_time_of_chain() + block.mining_time
         # edge case if we are adding genesis block
         else:
             block.timestamp = block.mining_time
@@ -22,25 +23,28 @@ class Blockchain:
     def pop(self) -> Block:
         return self.chain.pop()
 
-    def removeBlocksFromEnd(self, numBlocks: int) -> None:
-        for i in range(numBlocks):
+    def remove_blocks_from_end(self, num_blocks: int) -> None:
+        for i in range(num_blocks):
             self.chain.pop()
 
-    def getGlobalTimeOfChain(self) -> float:
+    def get_global_time_of_chain(self) -> float:
         # check that chain is non-empty
         if len(self.chain) == 0:
             return 0.0
         else:
             return self.chain[-1].timestamp
 
-    def getBlock(self, index: int) -> Block:
+    def get_block_at_index(self, index: int) -> Block:
         return self.chain[index]
+
+    def __len__(self):
+        return len(self.chain)
 
     def __str__(self):
         output = []
         for i in range(len(self.chain)):
             output.append("Block: {}".format(i))
-            output.append("*"*40)
+            output.append("*" * 40)
             output.append(self.chain[i].__str__())
             output.append("-" * 40)
         return "\n".join(output)
--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+
 def get_block_time(alpha, difficulty, gamma=0, _type='selfish'):
-    '''
-    _type takes 4 options to get the block mining time taken by: 
-        'selfish' : selfish miner
-        'honest' : honest miner
-        'defect' : the proportion of of honest miners who switch
-        'honest_fork' : remaining honest miners after defectors leave
-    '''
+    """
+    _type takes 4 options to get the block mining time taken by:
+    'selfish' : selfish miner
+    'honest' : honest miner
+    'defect' : the proportion of of honest miners who switch
+    'honest_fork' : remaining honest miners after defectors leave
+    """
     if _type == 'selfish':   
         return np.random.exponential(1/alpha*10/difficulty)
     elif _type == 'honest':
