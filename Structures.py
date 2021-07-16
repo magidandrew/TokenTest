bb85e597ff54910ce3f347944731e6c06e01dc5c
Andrew Magid <magid.andrew@gmail.com>
2021-07-16 16:09:42 -0400

    bug fix in Structures file

--- Structures.py
+++ Structures.py
@@ -1,7 +1,8 @@
 class Block:
-    def __init__(self, mining_time: float = 0, timestamp: float = 0):
-        self.mining_time = mining_time
-        self.timestamp = timestamp
+    def __init__(self, mining_time: float = 0):
+        self.mining_time: float = mining_time
+        # timestamp is calculated when a block is added to the chain
+        self.timestamp: float = 0
 
     def __str__(self):
         return "\n".join(["mining time: {}".format(self.mining_time), "timestamp: {}".format(self.timestamp)])
@@ -11,6 +12,11 @@ class Blockchain:
         self.chain: list[Block] = []
 
     def addBlock(self, block: Block):
+        if len(self.chain) != 0:
+            block.timestamp = self.getGlobalTimeOfChain() + block.mining_time
+        # edge case if we are adding genesis block
+        else:
+            block.timestamp = block.mining_time
         self.chain.append(block)
 
     def pop(self) -> Block:
@@ -21,7 +27,11 @@ class Blockchain:
             self.chain.pop()
 
     def getGlobalTimeOfChain(self) -> float:
-        return self.chain[-1].timestamp
+        # check that chain is non-empty
+        if len(self.chain) == 0:
+            return 0.0
+        else:
+            return self.chain[-1].timestamp
 
     def getBlock(self, index: int) -> Block:
         return self.chain[index]
@@ -32,4 +42,5 @@ class Blockchain:
             output.append("Block: {}".format(i))
             output.append("*"*40)
             output.append(self.chain[i].__str__())
+            output.append("-" * 40)
         return "\n".join(output)
