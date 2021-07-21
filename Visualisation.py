8cb22ab2a32f829d1d4787581dcd9bb570ddd17c
AochongLi <al4143@columbia.edu>
2021-07-21 06:38:30 -0400

    add some functions

--- Visualisation.py
+++ Visualisation.py
@@ -2,3 +2,12 @@ import numpy as np
 import matplotlib as plt
 from Structures import Block, Blockchain
 import utils
+
+
+def revenue_cost_time(revenue: list, cost: list) -> None:
+
+
+def difficulty_time() -> None:
+
+
+def alpha_revenue() -> None:
