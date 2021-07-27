091dcee05d28620a7a0d6f988812894112fe27ac
ketanjog <kj2473@columbia.edu>
2021-07-27 08:18:43 -0400

    Smart Agent class

---
+++ Structure/SmartAgent.py
@@ -0,0 +1,14 @@
+from Structure.AbstractAgent import AbstractAgent
+import numpy as np
+
+
+class SmartAgent(AbstractAgent):
+
+    def __init__(self, alpha: float, is_mining: bool):
+        super().__init__(alpha)
+        self.is_mining = True
+
+
+    def get_block_time(self, difficulty: float) -> float:
+        difficulty_scaling = 10 * difficulty
+        return np.random.exponential(1 / (1 - self.alpha) * difficulty_scaling)
