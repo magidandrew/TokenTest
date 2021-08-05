09da9733bb1d10dad168d77476be125045c25f8a
AochongLi <al4143@columbia.edu>
2021-08-05 05:17:57 -0400

    visual revenue_cost work

--- Visualisation.py
+++ Visualisation.py
@@ -1,71 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Structure.Blockchain import Blockchain
-from Structure.Block import Block
-import utils
-
-# all paramters for reference
-'''self.__nb_simulations = kwargs['nb_simulations']
-self.__delta = 0  # advance of selfish miners on honest ones
-self.__honest_valid_blocks = 0
-self.__selfish_valid_blocks = 0
-self.__blockchain = Blockchain()
-self.__alpha = kwargs['alpha']
-self.__gamma = kwargs['gamma']
-self.__revenue_ratio = None
-self.__orphan_blocks = 0
-self.__total_validated_blocks = 0
-self.__honest_reward = 0
-self.__selfish_reward = 0
-self.__Tho = 10
-self.__window_size = 2016
-self.__breaktime = None
-self.__Sn0 = None
-self.__B = 1
-self.__all_blocks_mined = []'''
+# from Structure.Blockchain import Blockchain
+# from Structure.Block import Block
+# import utils
 
 
 def revenue_cost_time(nb_simulations: int, strat: str, revenue: list, cost: list, alpha: float, gamma: float, window_size: int = 2016) -> None:
     # Derive the list of epochs
-    block_periods = [self.__window_size for x in range(0, self.__nb_simulations // self.__window_size)] \
-        + [self.__nb_simulations % self.__window_size]
+    block_periods = [window_size for x in range(0, nb_simulations // window_size)] \
+        + [nb_simulations % window_size]
     # Make the list of epoch to be strings
-    for i in len(block_periods):
+    for i in range(len(block_periods)):
         if block_periods[i] == 2016:
-            block_periods[i] = "epoch" + str(i) + ": complete"
+            block_periods[i] = "epoch" + str(i+1) + ": complete"
         else:
-            block_periods[i] = "epoch" + str(i) + ": incomplete"
+            block_periods[i] = "epoch" + str(i+1) + ": incomplete"
 
+    # print(block_periods)
+    plt.figure(figsize=[6.4, 15.8])
     # Revenue v.s. Epoch
-    plt.bar(revenue, block_periods, color="red", width=0.4,
+    plt.bar(block_periods, revenue, color="red", width=0.4,
             label="alpha: " + str(alpha) + " gamma: " + str(gamma))
 
+    for i in range(len(block_periods)):
+        plt.text(i, revenue[i], revenue[i], ha="center", va="bottom")
+
     plt.xlabel("Epochs ({} blocks / epoch)".format(window_size))
+    plt.xticks(rotation=15)
     plt.ylabel("Revenue")
     plt.title("Revenue of {} v.s. Time".format(strat))
     plt.legend()
     plt.show()
 
     # Cost v.s. Epoch
-    plt.bar(cost, block_periods, color="red", width=0.4,
+    plt.bar(block_periods, cost, color="green", width=0.4,
             label="alpha: " + str(alpha) + " gamma: " + str(gamma))
 
+    for i in range(len(block_periods)):
+        plt.text(i, cost[i], cost[i], ha="center", va="bottom")
+
     plt.xlabel("Epochs ({} blocks / epoch)".format(window_size))
+    plt.xticks(rotation=15)
     plt.ylabel("Cost")
     plt.title("Cost of {} v.s. Time".format(strat))
     plt.legend()
     plt.show()
 
-    gross = revenue - cost
+    gross = [revenue[i] - cost[i] for i in range(len(revenue))]
     # Gross v.s. Epoch
-    plt.bar(gross, block_periods, color="red", width=0.4,
-            label="alpha: " + str(alpha) + " gamma: " + str(gamma))
+    gross_pos = [gross[i] if gross[i] > 0 else 0 for i in range(len(gross))]
+    gross_neg = [gross[i] if gross[i] < 0 else 0 for i in range(len(gross))]
+
+    plt.bar(block_periods, gross_pos, color='red', width=0.4,
+            label="Positive Profit with " + "alpha: " + str(alpha) + " gamma: " + str(gamma))
+    plt.bar(block_periods, gross_neg, color='green', width=0.4,
+            label="Negative Profit with "+"alpha: " + str(alpha) + " gamma: " + str(gamma))
+
+    for i in range(len(block_periods)):
+        plt.text(i, gross[i], gross[i], ha="center", va="bottom")
 
     plt.xlabel("Epochs ({} blocks / epoch)".format(window_size))
+    plt.axhline(y=0, color='black', linestyle='-')
+    plt.xticks(rotation=15)
     plt.ylabel("Gross Profit")
     plt.title("Gross Profit of {} v.s. Time".format(strat))
     plt.legend()
     plt.show()
+
+
+'''
 
 
 def difficulty_time(nb_simulations: int, strat: str, B_list: list, alpha: float, gamma: float, window_size: int = 2016) -> None:
@@ -118,3 +121,4 @@ def alpha_revenue(strat: str, revenue_mean_list: list, cost_mean_list: list, alp
     plt.title("Gross Profit of {} v.s. Alpha Level".format(strat))
     plt.legend()
     plt.show()
+'''
